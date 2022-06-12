d0e670e4c954319f86763616094843f0cd8b62a9
Fabusim <106881541+Fabusim@users.noreply.github.com>
2022-06-12 10:36:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /**
-   * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288292024190
-   * Follow https://github.com/DikaArdnt
+   * Create By Dms_Owner.
+   * Contact Me on wa.me/6285817421330
+   * Follow https://github.com
 */
 
 const fs = require('fs')
@@ -18,9 +18,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['6285817421330']
+global.premium = ['6285817421330']
+global.packname = 'Dms_Owner'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
